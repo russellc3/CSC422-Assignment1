bc7d16572a072600a731e30a729e07b4b04b7f7b
russellc3 <77652047+russellc3@users.noreply.github.com>
2021-05-15 14:21:01 -0700

    Release 2
    Added the ability to search for pets by name or age.

--- src/PetDatabase.java
+++ src/PetDatabase.java
@@ -9,9 +9,9 @@ public class PetDatabase {
 	
 	public static void main(String[] args) {
 		
-		petDatabase.add(new Pet("Fido", 2));
-		petDatabase.add(new Pet("Mittens", 5));
-		petDatabase.add(new Pet("Spike", 4));
+		petDatabase.add(new Pet("fido", 2));
+		petDatabase.add(new Pet("mittens", 5));
+		petDatabase.add(new Pet("spike", 4));
 		
 		int menuSelection = 0;
 		boolean validInput = false;
@@ -61,8 +61,10 @@ public class PetDatabase {
 				case 4:
 					break;
 				case 5:
+					nameSearch();
 					break;
 				case 6:
+					ageSearch();
 					break;
 				case 7:
 					break;
@@ -71,7 +73,26 @@ public class PetDatabase {
 		}
 	}
 	
-	public static void addPet() {
+	public static void printPets() {
+		
+		System.out.print("\n+----------------------+");
+		System.out.print("\n| ID | NAME      | AGE |");
+		System.out.print("\n+----------------------+");
+		for (int i = 0; i < petDatabase.size(); i++) {
+			System.out.print("\n");
+			System.out.print("|");
+			System.out.printf("%3s", i);
+			System.out.print(" | ");
+			System.out.printf("%-9s", petDatabase.get(i).getName());
+			System.out.print(" |");
+			System.out.printf("%4s", petDatabase.get(i).getAge());
+			System.out.print(" |");
+		}
+		System.out.print("\n+----------------------+\n");
+		System.out.print(petDatabase.size() + " rows in set.\n");
+	}
+	
+	public static void addPet () {
 		
 		String line = "";
 		String name = "";
@@ -94,26 +115,69 @@ public class PetDatabase {
 		System.out.print("\n" + numPetsAdded + " pets added.\n");
 	}
 	
-	public static void printPets() {
+	public static void nameSearch () {
+		
+		String nameToSearch = "";
+		int numMatches = 0;
+		
+		System.out.print("\nEnter a name to search: ");
+		
+		nameToSearch = input.next().toLowerCase();
 		
 		System.out.print("\n+----------------------+");
 		System.out.print("\n| ID | NAME      | AGE |");
 		System.out.print("\n+----------------------+");
+		
 		for (int i = 0; i < petDatabase.size(); i++) {
-			System.out.print("\n");
-			System.out.print("|");
-			System.out.printf("%3s", i);
-			System.out.print(" | ");
-			System.out.printf("%-9s", petDatabase.get(i).getName());
-			System.out.print(" |");
-			System.out.printf("%4s", petDatabase.get(i).getAge());
-			System.out.print(" |");
+			if (nameToSearch.equals(petDatabase.get(i).getName())) {
+				System.out.print("\n");
+				System.out.print("|");
+				System.out.printf("%3s", i);
+				System.out.print(" | ");
+				System.out.printf("%-9s", petDatabase.get(i).getName());
+				System.out.print(" |");
+				System.out.printf("%4s", petDatabase.get(i).getAge());
+				System.out.print(" |");
+				numMatches++;
+			}
 		}
 		System.out.print("\n+----------------------+\n");
-		System.out.print(petDatabase.size() + " rows in set.\n");
+		System.out.print(numMatches + " rows in set.\n");
+		
 	}
 	
-	public static void printMenu() {
+	public static void ageSearch () {
+		
+		int ageToSearch = 0;
+		int numMatches = 0;
+		
+		System.out.print("\nEnter age to search: ");
+		
+		ageToSearch = input.nextInt();
+		
+
+		System.out.print("\n+----------------------+");
+		System.out.print("\n| ID | NAME      | AGE |");
+		System.out.print("\n+----------------------+");
+		
+		for (int i = 0; i < petDatabase.size(); i++) {
+			if (ageToSearch == petDatabase.get(i).getAge()) {
+				System.out.print("\n");
+				System.out.print("|");
+				System.out.printf("%3s", i);
+				System.out.print(" | ");
+				System.out.printf("%-9s", petDatabase.get(i).getName());
+				System.out.print(" |");
+				System.out.printf("%4s", petDatabase.get(i).getAge());
+				System.out.print(" |");
+				numMatches++;
+			}
+		}
+		System.out.print("\n+----------------------+\n");
+		System.out.print(numMatches + " rows in set.\n");
+	}
+	
+	public static void printMenu () {
 		System.out.print("\nWhat would you like to do?\n"
 				+ "1) View all pets\n"
 				+ "2) Add more pets\n"
